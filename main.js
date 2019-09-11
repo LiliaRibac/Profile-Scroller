6d724b6cb7fc923723c6457f6feb1ba4cb21d4ab
LiliaRibac <ribac.lilia@gmai.com>
2019-09-11 13:24:20 -0700

    fix reload

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const data =[
-  {
+const data = [{
     name: "John Doe",
     age: 30,
     gender: 'male',
     lokingfor: "female",
     location: "Boston MA",
-    image: 'https://randomuser.me/api/portraits/men/85.jpg'
+    image: 'https://randomuser.me/api/portraits/men/83.jpg'
   },
   {
     name: "Jen Smith",
@@ -32,35 +31,46 @@ const data =[
   }
 ];
 
-const profiles = profileIterator(data)
+const profiles = profileIterator(data);
+
+nextProfile();
 
 //Next Event
 document.getElementById('next').addEventListener("click", nextProfile);
 
 //Next Profile display
-function nextProfile(){
+function nextProfile() {
   const currentProfile = profiles.next().value;
 
-  document.getElementById('profileDisplay').innerHTML =`
-  <ul class="list-group">
-  <li class="list-group-item">Name: ${currentProfile.name}</li>
-  <li class="list-group-item">Age: ${currentProfile.age}</li>
-  <li class="list-group-item">Location: ${currentProfile.location}</li>
-  <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lokingfor}</li>
-  </ul>`
+  if (currentProfile !== undefined) {
+    document.getElementById('profileDisplay').innerHTML = `
+     <ul class="list-group">
+        <li class="list-group-item">Name: ${currentProfile.name}</li>
+        <li class="list-group-item">Age: ${currentProfile.age}</li>
+        <li class="list-group-item">Location: ${currentProfile.location}</li>
+        <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lokingfor}</li>
+     </ul>`
 
-  document.getElementById("imgDisplay").innerHTML = `<img src=" ${currentProfile.image}">`
+    document.getElementById("imgDisplay").innerHTML = `<img src=" ${currentProfile.image}">`
+  } else {
+    //No more profile
+    window.location.reload()
+  }
 }
 
+
 //Profile Iterator
-function profileIterator(profiles){
+function profileIterator(profiles) {
   let nextIndex = 0
 
-  return{
-    next: function(){
-      return nextIndex < profiles.length ?
-      {value: profiles[nextIndex++], done: false}:
-      {done: true}
+  return {
+    next: function () {
+      return nextIndex < profiles.length ? {
+        value: profiles[nextIndex++],
+        done: false
+      } : {
+        done: true
+      }
     }
   };
 }
