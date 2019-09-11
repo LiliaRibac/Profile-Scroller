9bd408078185e9c123f300455a16422dd77ccfae
LiliaRibac <ribac.lilia@gmai.com>
2019-09-11 12:10:01 -0700

    second commit main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+const data =[
+  {
+    name: "John Doe",
+    age: 30,
+    gender: 'male',
+    lokingfor: "female",
+    location: "Boston MA",
+    image: 'https://randomuser.me/api/portraits/men/85.jpg'
+  },
+  {
+    name: "Jen Smith",
+    age: 326,
+    gender: 'female',
+    lokingfor: "male",
+    location: "Lynn MA",
+    image: 'https://randomuser.me/api/portraits/men/82.jpg'
+  },
+  {
+    name: "William Johnson",
+    age: 38,
+    gender: 'male',
+    lokingfor: "female",
+    location: "Miami FL",
+    image: 'https://randomuser.me/api/portraits/men/10.jpg'
+  }, {
+    name: "Emma Avery",
+    age: 27,
+    gender: 'female',
+    lokingfor: "male",
+    location: "New York  NY",
+    image: 'https://randomuser.me/api/portraits/men/25.jpg'
+  }
+]
