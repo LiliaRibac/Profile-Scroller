bf8941bbdda4716b5ca169bdd948a72252e4af1d
LiliaRibac <ribac.lilia@gmai.com>
2019-09-11 12:57:09 -0700

    add profileIterator to see it in your browser

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const data =[
     gender: 'female',
     lokingfor: "male",
     location: "Lynn MA",
-    image: 'https://randomuser.me/api/portraits/men/82.jpg'
+    image: 'https://randomuser.me/api/portraits/women/82.jpg'
   },
   {
     name: "William Johnson",
@@ -28,6 +28,39 @@ const data =[
     gender: 'female',
     lokingfor: "male",
     location: "New York  NY",
-    image: 'https://randomuser.me/api/portraits/men/25.jpg'
+    image: 'https://randomuser.me/api/portraits/women/25.jpg'
   }
-]
+];
+
+const profiles = profileIterator(data)
+
+//Next Event
+document.getElementById('next').addEventListener("click", nextProfile);
+
+//Next Profile display
+function nextProfile(){
+  const currentProfile = profiles.next().value;
+
+  document.getElementById('profileDisplay').innerHTML =`
+  <ul class="list-group">
+  <li class="list-group-item">Name: ${currentProfile.name}</li>
+  <li class="list-group-item">Age: ${currentProfile.age}</li>
+  <li class="list-group-item">Location: ${currentProfile.location}</li>
+  <li class="list-group-item">Preference: ${currentProfile.gender} looking for ${currentProfile.lokingfor}</li>
+  </ul>`
+
+  document.getElementById("imgDisplay").innerHTML = `<img src=" ${currentProfile.image}">`
+}
+
+//Profile Iterator
+function profileIterator(profiles){
+  let nextIndex = 0
+
+  return{
+    next: function(){
+      return nextIndex < profiles.length ?
+      {value: profiles[nextIndex++], done: false}:
+      {done: true}
+    }
+  };
+}
